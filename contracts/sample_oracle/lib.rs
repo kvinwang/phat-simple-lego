807e1aa23b7cab4e40b89d0c63317a4ca4117f5b
Kevin Wang <wy721@qq.com>
2023-01-11 03:29:01 +0000

    Setup dev env

---
+++ contracts/sample_oracle/lib.rs
@@ -0,0 +1,113 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+
+use ink_lang as ink;
+
+pub use crate::sample_oracle::*;
+
+#[ink::contract(env = pink_extension::PinkEnvironment)]
+mod sample_oracle {
+    use alloc::string::String;
+    use ink_storage::traits::{PackedLayout, SpreadLayout};
+    use pink_extension as pink;
+    use primitive_types::H160;
+    use scale::{Decode, Encode};
+
+    /// Defines the storage of your contract.
+    /// Add new fields to the below struct in order
+    /// to add new static storage fields to your contract.
+    #[ink(storage)]
+    pub struct SampleOracle {
+        owner: AccountId,
+        config: Option<Config>,
+    }
+
+    #[derive(Encode, Decode, Debug, PackedLayout, SpreadLayout)]
+    #[cfg_attr(
+        feature = "std",
+        derive(scale_info::TypeInfo, ink_storage::traits::StorageLayout)
+    )]
+    struct Config {
+        rpc: String,
+        anchor: [u8; 20],
+    }
+
+    #[derive(Encode, Decode, Debug)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        BadOrigin,
+        NotConfigurated,
+        BadAbi,
+        FailedToGetStorage,
+        FailedToDecodeStorage,
+        FailedToEstimateGas,
+        FailedToCreateRollupSession,
+        FailedToFetchLock,
+        FailedToReadQueueHead,
+    }
+
+    type Result<T> = core::result::Result<T, Error>;
+
+    impl SampleOracle {
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self {
+                owner: Self::env().caller(),
+                config: None,
+            }
+        }
+
+        /// Gets the owner of the contract
+        #[ink(message)]
+        pub fn owner(&self) -> AccountId {
+            self.owner
+        }
+
+        /// Configures the rollup target
+        #[ink(message)]
+        pub fn config(&mut self, rpc: String, anchor: H160) -> Result<()> {
+            self.ensure_owner()?;
+            self.config = Some(Config {
+                rpc,
+                anchor: anchor.into(),
+            });
+            Ok(())
+        }
+
+        // Returning `()` as a workaround for https://github.com/l00k/devphase/issues/19
+        #[ink(message)]
+        pub fn handle_req(&self, eth_usd_price: u128) -> () {
+            pink::info!("handle_req(price={eth_usd_price})");
+        }
+
+        /// Returns BadOrigin error if the caller is not the owner
+        fn ensure_owner(&self) -> Result<()> {
+            if self.env().caller() == self.owner {
+                Ok(())
+            } else {
+                Err(Error::BadOrigin)
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn default_works() {
+            let _ = env_logger::try_init();
+            pink_extension_runtime::mock_ext::mock_all_ext();
+
+            let (rpc, anchor_addr) = consts();
+
+            let mut sample_oracle = SampleOracle::default();
+            sample_oracle.config(rpc, anchor_addr).unwrap();
+
+            let res = sample_oracle.handle_req().unwrap();
+            println!("res: {:#?}", res);
+        }
+    }
+}
